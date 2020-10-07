c4be492243b0d0354af43e3a63b5b125640b2f56
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 15:03:47 +0700

    feat(core): add child modular to core module

--- module/core/lib/core.dart
+++ module/core/lib/core.dart
@@ -1,7 +1,10 @@
-library core;
+import 'package:flutter_modular/flutter_modular.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class CoreModule extends ChildModule{
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRouter> get routers => [];
+
 }
