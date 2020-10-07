db46e884916d1996f338f752070418809283d1d9
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 18:15:31 +0700

    feat(feature-game): implement get game list bloc to home screen

--- module/feature_game/lib/presentation/pages/home_screen.dart
+++ module/feature_game/lib/presentation/pages/home_screen.dart
@@ -1,15 +1,31 @@
+import 'package:feature_game/presentation/bloc/game_bloc.dart';
+import 'package:feature_game/presentation/bloc/game_event.dart';
+import 'package:feature_game/presentation/bloc/game_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-
-        ],
+    return BlocProvider(
+      create: (context) => Modular.get<GameBLoc>()..add(LoadGameList()),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: BlocBuilder<GameBLoc, GameState>(builder: (context, state) {
+          if (state is GameLoadedState) {
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: state.gameResponse.results.length,
+                itemBuilder: (context, index) {
+                  return Text(state.gameResponse.results[index].name);
+                });
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }),
       ),
     );
   }
