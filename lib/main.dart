ae1514b760ea3cddd22db73e877165a51ed5f93c
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 16:16:40 +0700

    feat(flutter-catalogue-game): inject splash bloc to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:feature_game/feature_game.dart';
 import 'package:flutter/material.dart';
 import 'package:core/external/route_strings.dart';
+import 'package:flutter_game_catalogue/presentation/blocs/splash_bloc.dart';
 import 'package:flutter_game_catalogue/presentation/pages/splash_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,23 +11,27 @@ void main() {
   ));
 }
 
-class AppModule extends MainModule{
+class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-    Bind((_) => RouteStrings()),
-  ];
+        Bind((_) => RouteStrings()),
+        Bind((_) => SplashBloc()),
+      ];
 
   @override
   Widget get bootstrap => MyApp();
 
   @override
   List<ModularRouter> get routers => [
-    ModularRouter(
-      Modular.get<RouteStrings>().splashScreenRoute,
-      child: (context, args) => SplashScreen(),
-    ),
-  ];
-
+        ModularRouter(
+          Modular.get<RouteStrings>().splashScreenRoute,
+          child: (context, args) => SplashScreen(),
+        ),
+        ModularRouter(
+          Modular.get<RouteStrings>().homeScreenRoute,
+          module: FeatureGameModule(),
+        ),
+      ];
 }
 
 class MyApp extends StatelessWidget {
