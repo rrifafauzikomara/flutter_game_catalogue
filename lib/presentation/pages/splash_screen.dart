e0bf8d62807ec42e05bfbe4501b0b1a50dc502a8
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 16:31:56 +0700

    feat(flutter-catalogue-game): implement splash bloc to splash screen

--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_game_catalogue/domain/routers/splash_router.dart';
 import 'package:flutter_game_catalogue/external/image_strings.dart';
+import 'package:flutter_game_catalogue/presentation/blocs/splash_bloc.dart';
+import 'package:flutter_game_catalogue/presentation/blocs/splash_event.dart';
+import 'package:flutter_game_catalogue/presentation/blocs/splash_state.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final SplashRouter _splashRouter = SplashRouterImpl();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(ImageStrings.logo,scale: 3.0,),
+    return BlocProvider(
+      create: (context) => Modular.get<SplashBloc>()..add(LoadSplashScreen()),
+      child: BlocListener<SplashBloc, SplashState>(
+        listener: (context, state) {
+          if (state is SplashLoadedState) {
+            _splashRouter.goToHomeScreen();
+          }
+        },
+        child: Scaffold(
+          body: BlocBuilder<SplashBloc, SplashState>(
+            builder: (context, state) => Center(
+              child: Image.asset(
+                ImageStrings.logo,
+                scale: 3.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
