428533b27d5cfa9a52ed9623336681ac116bbf4e
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 16:12:09 +0700

    feat(feature-game): add home screen

---
+++ module/feature_game/lib/presentation/pages/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
