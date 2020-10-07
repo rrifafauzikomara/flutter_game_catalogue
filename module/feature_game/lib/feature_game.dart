1a66868c86e202fea49a35a69563653f5cd9de2d
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-07 18:15:03 +0700

    feat(feature-game): inject get game list in feature game module

--- module/feature_game/lib/feature_game.dart
+++ module/feature_game/lib/feature_game.dart
@@ -1,9 +1,25 @@
+import 'package:core/external/api_constant.dart';
+import 'package:dio/dio.dart';
+import 'package:feature_game/data/remote/dataresources/game_api_provider.dart';
+import 'package:feature_game/data/remote/repositories/game_repository_impl.dart';
+import 'package:feature_game/domain/usecases/game_use_case.dart';
+import 'package:feature_game/presentation/bloc/game_bloc.dart';
 import 'package:feature_game/presentation/pages/home_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class FeatureGameModule extends ChildModule {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind((_) => ApiConstant()),
+        Bind((_) =>
+            Dio(BaseOptions(baseUrl: Modular.get<ApiConstant>().baseUrl))),
+        Bind((_) => GameApiProviderImpl(dio: Modular.get<Dio>())),
+        Bind((_) => GameRepositoryImpl(
+            gameApiProvider: Modular.get<GameApiProviderImpl>())),
+        Bind((_) =>
+            GameUseCaseImpl(gameRepository: Modular.get<GameRepositoryImpl>())),
+        Bind((_) => GameBLoc(gameUseCase: Modular.get<GameUseCaseImpl>())),
+      ];
 
   @override
   List<ModularRouter> get routers => [
