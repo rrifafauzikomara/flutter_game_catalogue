2920ce36c4bda14834993e7495cc057fbfd97cc2
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-11 23:29:01 +0700

    feat(flutter-game-catalogue): add feature home module

---
+++ module/feature_home/lib/feature_home.dart
@@ -0,0 +1,38 @@
+import 'package:core/external/api_constant.dart';
+import 'package:core/external/route_strings.dart';
+import 'package:dio/dio.dart';
+import 'package:feature_contributor/presentation/pages/profile_screen.dart';
+import 'package:feature_game/data/remote/dataresources/game_api_provider.dart';
+import 'package:feature_game/data/remote/repositories/game_repository_impl.dart';
+import 'package:feature_game/domain/usecases/game_use_case.dart';
+import 'package:feature_game/presentation/bloc/game_bloc.dart';
+import 'package:feature_game/presentation/pages/game_list_screen.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'presentation/pages/home_screen.dart';
+
+class FeatureHomeModule extends ChildModule {
+  @override
+  List<Bind> get binds => [
+    Bind((_) => RouteStrings()),
+    Bind((_) => GameListScreen()),
+    Bind((_) => ProfileScreen()),
+    Bind((_) => ApiConstant()),
+    Bind((_) =>
+        Dio(BaseOptions(baseUrl: Modular.get<ApiConstant>().baseUrl))),
+    Bind((_) => GameApiProviderImpl(dio: Modular.get<Dio>())),
+    Bind((_) => GameRepositoryImpl(
+        gameApiProvider: Modular.get<GameApiProviderImpl>())),
+    Bind((_) =>
+        GameUseCaseImpl(gameRepository: Modular.get<GameRepositoryImpl>())),
+    Bind((_) => GameBLoc(gameUseCase: Modular.get<GameUseCaseImpl>())),
+  ];
+
+  @override
+  List<ModularRouter> get routers => [
+    ModularRouter(
+      '/',
+      child: (_, __) => HomeScreen(),
+    ),
+  ];
+}
