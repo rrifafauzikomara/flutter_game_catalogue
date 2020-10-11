2eb256f66e433fda3c80a5bd1e61e69af0da5340
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-11 23:30:54 +0700

    feat(feature-game-catalogue): add feature home module to main app and change direct from splash to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:feature_contributor/feature_contributor.dart';
 import 'package:feature_game/feature_game.dart';
 import 'package:flutter/material.dart';
 import 'package:core/external/route_strings.dart';
 import 'package:flutter_game_catalogue/presentation/blocs/splash_bloc.dart';
 import 'package:flutter_game_catalogue/presentation/pages/splash_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:feature_home/feature_home.dart';
 
 void main() {
   runApp(ModularApp(
@@ -28,8 +30,16 @@ class AppModule extends MainModule {
           child: (context, args) => SplashScreen(),
         ),
         ModularRouter(
-          Modular.get<RouteStrings>().homeScreenRoute,
+          Modular.get<RouteStrings>().gameListRoute,
           module: FeatureGameModule(),
+        ),
+        ModularRouter(
+          Modular.get<RouteStrings>().homeScreenRoute,
+          module: FeatureHomeModule(),
+        ),
+        ModularRouter(
+          Modular.get<RouteStrings>().profileRoute,
+          module: FeatureContributorModule(),
         ),
       ];
 }
--- module/feature_game/lib/presentation/pages/game_list_screen.dart
+++ module/feature_game/lib/presentation/pages/game_list_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class HomeScreen extends StatelessWidget {
+class GameListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
