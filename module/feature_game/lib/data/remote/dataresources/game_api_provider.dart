b3c5353c85b1824b6c8d217fcae2bb67ffaf55a8
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-14 23:03:59 +0700

    feat(feature-game): add get detail game

--- module/feature_game/lib/data/remote/dataresources/game_api_provider.dart
+++ module/feature_game/lib/data/remote/dataresources/game_api_provider.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:feature_game/domain/entities/game_detail_response.dart';
 import 'package:feature_game/domain/entities/game_response.dart';
 import 'package:flutter/foundation.dart';
 
 abstract class GameApiProvider {
   Future<GameResponse> getGameList();
+
+  Future<GameDetailResponse> getDetailGame(String gameId);
 }
 
 class GameApiProviderImpl extends GameApiProvider {
@@ -16,6 +19,18 @@ class GameApiProviderImpl extends GameApiProvider {
     try {
       Response _response = await dio.get("/games?pages=1&page_size=20");
       GameResponse _gameResponse = GameResponse.fromJson(_response.data);
+      return _gameResponse;
+    } on DioError catch (e) {
+      return e.error;
+    }
+  }
+
+  @override
+  Future<GameDetailResponse> getDetailGame(String gameId) async {
+    try {
+      Response _response = await dio.get("/games/$gameId");
+      GameDetailResponse _gameResponse =
+          GameDetailResponse.fromJson(_response.data);
       return _gameResponse;
     } on DioError catch (e) {
       return e.error;
