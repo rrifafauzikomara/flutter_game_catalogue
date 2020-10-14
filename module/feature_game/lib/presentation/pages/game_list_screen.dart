816c12afcc979c47e0b6caabc5842a87aae20f3f
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-14 18:52:50 +0700

    feat(feature-game): update game list ui

--- module/feature_game/lib/presentation/pages/game_list_screen.dart
+++ module/feature_game/lib/presentation/pages/game_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:feature_game/external/date_formatter.dart';
 import 'package:feature_game/presentation/bloc/game_bloc.dart';
 import 'package:feature_game/presentation/bloc/game_event.dart';
 import 'package:feature_game/presentation/bloc/game_state.dart';
@@ -11,6 +12,7 @@ class GameListScreen extends StatelessWidget {
     return BlocProvider(
       create: (context) => Modular.get<GameBLoc>()..add(LoadGameList()),
       child: Scaffold(
+        backgroundColor: Colors.white38,
         appBar: AppBar(),
         body: BlocBuilder<GameBLoc, GameState>(builder: (context, state) {
           if (state is GameLoadedState) {
@@ -18,7 +20,89 @@ class GameListScreen extends StatelessWidget {
                 shrinkWrap: true,
                 itemCount: state.gameResponse.results.length,
                 itemBuilder: (context, index) {
-                  return Text(state.gameResponse.results[index].name);
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.9, 0.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.5),
+                          blurRadius: 1.0,
+                        ),
+                      ],
+                    ),
+                    child: InkWell(
+                      onTap: (){
+
+                      },
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.network(
+                              state.gameResponse.results[index].backgroundImage,
+                              height: 100.0,
+                              width: 100.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Text(
+                                    state.gameResponse.results[index].name,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8.0),
+                                    child: Text(
+                                        DateFormatter.dateFormatter(state
+                                            .gameResponse.results[index].released),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            color: Colors.lightBlue,
+                                            fontSize: 14.0)),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8.0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                          size: 16.0,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left:8.0),
+                                          child: Text(
+                                              "${state.gameResponse.results[index].rating}/5.00",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.normal,
+                                                  color: Colors.amber,
+                                                  fontSize: 14.0)),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
                 });
           } else {
             return Center(
