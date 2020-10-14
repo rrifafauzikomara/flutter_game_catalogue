2f3e8a4c5e1496d7bc2d7abf76232013f9f388d3
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-14 23:08:21 +0700

    feat(fcore): add game detail route strings

--- module/core/lib/external/route_strings.dart
+++ module/core/lib/external/route_strings.dart
@@ -2,5 +2,6 @@ class RouteStrings{
   String splashScreenRoute = "/splashScreen";
   String homeScreenRoute = "/homeScreen";
   String gameListRoute = "/gameListRoute";
+  String gameDetailRoute = "/gameDetailRoute";
   String profileRoute = "/profileRoute";
 }
