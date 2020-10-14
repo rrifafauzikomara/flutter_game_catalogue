d7fa72f98dd68b204e468d16c3bcf9f519021b1a
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-14 23:06:18 +0700

    feat(feature-game): add get game detail

--- module/feature_game/lib/domain/repositories/game_repository.dart
+++ module/feature_game/lib/domain/repositories/game_repository.dart
@@ -1,5 +1,8 @@
+import 'package:feature_game/domain/entities/game_detail_response.dart';
 import 'package:feature_game/domain/entities/game_response.dart';
 
 abstract class GameRepository {
   Future<GameResponse> getGameList();
+
+  Future<GameDetailResponse> getDetailGame(String gameId);
 }
